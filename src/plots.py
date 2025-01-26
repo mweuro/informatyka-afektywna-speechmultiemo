6ab50f19b9672a833dd2379a6f23cdeae8efe76b
mweuro <20mehow01@gmail.com>
2025-01-26 22:05:34 +0100

    finish

--- src/plots.py
+++ src/plots.py
@@ -15,17 +15,17 @@ def plot_metrics(train_losses, val_losses, train_accs, val_accs, train_precs, va
     axs[0][0].plot(epochs, val_losses, color = 'blue', linestyle = 'solid', label = 'val loss')
     axs[0][1].plot(epochs, train_accs, color = 'red', linestyle = 'dashdot', label = 'train acc')
     axs[0][1].plot(epochs, val_accs, color = 'red', linestyle = 'solid', label = 'val acc')
-    axs[1][0].plot(epochs, train_precs, color = 'green', linestyle = 'dashdot', label = 'train loss')
-    axs[1][0].plot(epochs, val_precs, color = 'green', linestyle = 'solid', label = 'val loss')
-    axs[1][1].plot(epochs, train_recs, color = 'gold', linestyle = 'dashdot', label = 'train acc')
-    axs[1][1].plot(epochs, val_recs, color = 'gold', linestyle = 'solid', label = 'val acc')
+    axs[1][0].plot(epochs, train_precs, color = 'green', linestyle = 'dashdot', label = 'train prec')
+    axs[1][0].plot(epochs, val_precs, color = 'green', linestyle = 'solid', label = 'val prec')
+    axs[1][1].plot(epochs, train_recs, color = 'gold', linestyle = 'dashdot', label = 'train rec')
+    axs[1][1].plot(epochs, val_recs, color = 'gold', linestyle = 'solid', label = 'val rec')
     
     for idx, (i, j) in enumerate(combinations):
         axs[i][j].set_title(titles[idx], fontsize = 10)
         axs[i][j].set_xlabel('Epoch')
         axs[i][j].set_ylabel(titles[idx])
+        axs[i][j].legend()
         
-    plt.legend()
     plt.suptitle('Model results', fontsize = 20)
     plt.tight_layout()
     plt.show()
@@ -38,10 +38,10 @@ def plot_metrics_late_fusion(losses, accs, precs, recs):
     titles = ['Loss', 'Accuracy', 'Precision', 'Recall']
     
     _, axs = plt.subplots(2, 2, figsize = (15, 10))
-    axs[0][0].plot(epochs, losses, color = 'blue', linestyle = 'losses', label = 'fusion loss')
-    axs[0][1].plot(epochs, accs, color = 'red', linestyle = 'losses', label = 'fusion acc')
-    axs[1][0].plot(epochs, precs, color = 'green', linestyle = 'losses', label = 'fusion prec')
-    axs[1][1].plot(epochs, recs, color = 'gold', linestyle = 'losses', label = 'train rec')
+    axs[0][0].plot(epochs, losses, color = 'blue', linestyle = 'solid', label = 'fusion loss')
+    axs[0][1].plot(epochs, accs, color = 'red', linestyle = 'solid', label = 'fusion acc')
+    axs[1][0].plot(epochs, precs, color = 'green', linestyle = 'solid', label = 'fusion prec')
+    axs[1][1].plot(epochs, recs, color = 'gold', linestyle = 'solid', label = 'fusion rec')
     
     
     for idx, (i, j) in enumerate(combinations):
